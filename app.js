90bf7c73c1df976617ad837f04efdbecb42c1459
Pankaj Rajendra Dube <dube.pankaj@Yash.com>
2024-10-10 09:37:41 +0530

    Pushing files

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 
-app.get("/", (req, res) => res.send("<h1>Hello from AWS ECS , CICD on ECS Implemented successfully </h1>"));
+app.get("/", (req, res) => res.send("<h1> CICD on ECS is Implemented successfully by Pankaj </h1>"));
 
 app.listen(80, () => {
   console.log("App listening on port 80!");
