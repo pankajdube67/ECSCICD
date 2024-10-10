8d216e2d0a1534ca79c5db0c762b1cc2d965a1a9
Pankaj Rajendra Dube <dube.pankaj@Yash.com>
2024-10-10 11:57:28 +0530

    Pushing files

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 
-app.get("/", (req, res) => res.send("<h1> CICD on ECS is Implemented Successfully  </h1>"));
+app.get("/", (req, res) => res.send("<h1> Thank you for attending AWS Training  </h1>"));
 
 app.listen(80, () => {
   console.log("App listening on port 80!");
